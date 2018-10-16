d17bbd03169abd0381bbe7a2237742069a4e4cbd
jaguirreayala <jaguirre-ayala@mail.csuchico.edu>
2018-10-15 17:39:35 -0700

    Can check if there are two arguments presented and continues to check if the image is valid and states the pixel width and height

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "Bitmap/Bitmap.h"
+#include "Bitmap/bitmap.h"
 #include <iostream>
 #include <vector>
 
@@ -8,6 +8,29 @@ using namespace std;
 //argv - each argument word, stored in an array
 int main(int argc, char* argv[])
 {
-  
+  string filename;
+  Bitmap image;
+
+  if(argc != 2)
+  {
+    cout << "Please specify one image file!\n";
+  }
+  else if(argc == 2)
+  {
+    filename = argv[1];
+
+    image.open(filename);
+
+    if(image.isImage())
+    {
+      vector <vector <Pixel> > imagePixels = image.toPixelMatrix();
+
+      cout << filename << " is " << imagePixels.size() << " pixels high and " << imagePixels[0].size() << " pixels wide\n";
+    }
+    else if(!image.isImage())
+    {
+      cout << "Image file must be a bitmap with 24-bit color depth. \n"; 
+    }
+  }
   return 0;
 }
